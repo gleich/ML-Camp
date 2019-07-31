acc32dc0a7e71e2dea5a3ceb12c0c88a5c127350
Matthew Gleich <matthewgleich@gmail.com>
2019-07-31 09:26:43 -0400

    Create Processing_Data.py

---
+++ notes/Processing_Data.py
@@ -0,0 +1,59 @@
+import tensorflow as tf
+import numpy as np
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+tf.reset_default_graph()
+
+image_height = 32
+image_width = 32
+
+color_channels = 3
+
+model_name = "cifar"
+
+
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+
+cifar_path = './cifar-10-data/'
+
+train_data = np.array([])
+train_labels = np.array([])
+
+# Load all the data batches.
+for i in range(1, 6):
+    data_batch = unpickle(cifar_path + 'data_batch_' + str(i))
+    train_data = np.append(train_data, data_batch[b'data'])
+    train_labels = np.append(train_labels, data_batch[b'labels'])
+
+
+# Load the eval batch.
+eval_batch = unpickle(cifar_path + 'test_batch')
+
+eval_data = eval_batch[b'data']
+eval_labels = eval_batch[b'labels']
+
+# Load the english category names.
+category_names_bytes = unpickle(cifar_path + 'batches.meta')[b'label_names']
+category_names = list(map(lambda x: x.decode("utf-8"), category_names_bytes))
+
+# TODO: Process Cifar data
+
+
+def process_data(data):
+    float_data = np.array(data, dtype=float) / 255.0
+
+    reshaped_data = np.reshape(
+        float_data, (-1, color_channels, image_height, image_width))
+
+    transposed_data = np.transpose(reshaped_data, [0, 2, 3, 1])
+    plt.imshow(transposed_data[0])
+
+
+process_data(train_data)
